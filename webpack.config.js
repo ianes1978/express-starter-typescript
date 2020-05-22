94ae2f023ca4c9425e7cc724ac58bcf1eb1e25b7
unknown <roberto.ianes@gmail.com>
2020-05-22 18:27:18 +0200

    start

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const { NODE_ENV = 'production' } = process.env;
+const nodeExternals = require('webpack-node-externals');
+module.exports = {
+  watch: NODE_ENV === 'development',
+  entry: './src/index.ts',
+  mode: NODE_ENV,
+  target: 'node',
+  externals: [nodeExternals()],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: ['ts-loader'],
+      },
+    ],
+  },
+};
